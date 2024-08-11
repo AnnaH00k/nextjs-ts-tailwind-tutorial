ee648e608d1b1c56aaab43f77c93256a9a410f7f
AnnaH00k <90142429+AnnaH00k@users.noreply.github.com>
2024-08-11 14:12:30 +0200

    Build main project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="min-h-[100vh] bg-[#444444] flex text-center items-center justify center">
+      <h1 className="text-4xl w-full  text-white">Hello World</h1>
+
+   </div>
+  );
+}
